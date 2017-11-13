920dfe459a17b122d99f2cf138b55853b94d55ef
wanger <1205722784@qq.com>
2017-11-13 16:31:31 +0800

    修改备注

--- src/resize.js
+++ src/resize.js
@@ -3,7 +3,7 @@
 
     /* 通过一个图片的url加载所需要的图片对象
      * url参数传入图片的url
-     * fn为回调方法返回一个Image对象
+     * fn为回调方法包含，一个Image对象的参数
      */
     methods.urltoImage = function (url,fn){
         var img = new Image();
@@ -13,7 +13,7 @@
         }
     };
 
-    /* 将一个Image对象压缩转变为一个Canvas类型对象
+    /* 将一个Image对象转变为一个Canvas类型对象
      * image参数传入一个Image对象
      */
     methods.imagetoCanvas = function (image){
@@ -28,7 +28,7 @@
     /* 将一个Canvas对象压缩转变为一个Blob类型对象
      * canvas参数传入一个Canvas对象
      * quality参数传入一个0-1的number类型，表示图片压缩质量
-     * fn为回调方法返回一个Blob类型文件
+     * fn为回调方法，包含一个Blob对象的参数
      */
     methods.canvasResizetoFile = function (canvas,quality,fn){
         canvas.toBlob(function(blob) {
@@ -46,7 +46,7 @@
 
     /* 将File（Blob）类型文件转变为dataURL字符串
      * file参数传入一个File（Blob）类型文件
-     * fn为回调方法返回一个dataURL字符串
+     * fn为回调方法，包含一个dataURL字符串的参数
      */
     methods.filetoDataURL = function(file,fn){
         var reader = new FileReader();
@@ -58,7 +58,7 @@
 
     /* 将一串dataURL字符串转变为Image类型文件
      * dataurl参数传入一个dataURL字符串
-     * fn为回调方法返回一个Image类型文件
+     * fn为回调方法，包含一个Image类型文件的参数
      */
     methods.dataURLtoImage = function(dataurl,fn){
         var img = new Image();
@@ -85,7 +85,7 @@
     /* 将File（Blob）类型文件压缩后再返回Blob类型对象
      * file参数传入一个File（Blob）类型文件
      * quality参数传入一个0-1的number类型，表示图片压缩质量
-     * fn为回调方法返回一个Blob类型文件
+     * fn为回调方法，包含一个Blob类型文件的参数
      * 使用示例：
      * var file = document.getElementById('demo').files[0];
      * fileResizetoFile(file,0.6,function(res){
