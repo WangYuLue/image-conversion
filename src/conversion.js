dd98a358dfa25aeb5af1092e766b337284214eb2
wangyulue <1206180097@qq.com>
2018-12-15 11:56:42 +0800

    fix head

--- src/conversion.js
+++ src/conversion.js
@@ -3,11 +3,11 @@
  * @author wangyulue(wangyulue@gmail.com)
  */
 
-(function (global, factory) {
+(function (factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
-        (global.imageConversion = factory());
-}(this, (function () {
+        (window.imageConversion = factory());
+}((function () {
     'use strict';
 
     const methods = {};
