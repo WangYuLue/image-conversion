8c52a66d9701eba4304706dd63e1d5fecf2aeb52
wangyulue <1206180097@qq.com>
2018-12-18 14:22:12 +0800

    add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp'),
-    babel = require("gulp-babel"),
-    uglify = require('gulp-uglify-es').default;
+const gulp = require('gulp');
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify-es').default;
 
 gulp.task('default', function () {
-gulp.src('src/*.js')
+  gulp.src('src/*.js')
     .pipe(babel())
     .pipe(uglify())
     .pipe(gulp.dest('build'))
